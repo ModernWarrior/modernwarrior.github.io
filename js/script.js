9e942328a5c45de1c49f6226d2041bebdec19786
ModernWarrior <modernwarriorapp@gmail.com>
2016-03-20 19:37:15 -0400

    Updated 3/20

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-$(document).ready(function(){
-	//Level 1 is to indicate if was complete or not
-	// var level1 = false;
-	// var level2 = false;
-	//sets level
 
-	// get the local storage user
+$(document).ready(function(){
+
+	/**GLOBAL VARIABLES**/
 	var currentUser = getUser();
 	var paused = false;
+	var stopped = false;
 	var cycle = 0;
 	var currentExercise = 0;
 	var delaySwitch = false;
@@ -22,9 +20,99 @@ $(document).ready(function(){
 	var prepare = false;
 	var runWorkout = 1;
 
-	// var isPaused = true;
+	/**USER SET UP**/
+	function updateUser(setting,value){
+		var user = JSON.parse(localStorage.getItem('user'));
+		if (!user){
+	 		user = {};
+	 	}
+	 	user[setting] = value; // user.mood
+	 	localStorage.setItem('user', JSON.stringify(user));
+	 }
+
+	function getUser(){
+	 	var user = JSON.parse(localStorage.getItem('user'));
+	 	if (!user){
+	 		return {};
+	 	}
+	 	return user;
+	 }
+
 	
-	// if the current user has a saved level, restore it
+	//Time Calculator 
+	function getTimeRemaining(endtime){
+		//Date.parse is converting to milliseconds. Essentially I'm trying to build a calculator with the var time
+		 var time = timer;
+		var seconds = Math.floor( (timer/1000) % 60 );
+		var minutes = Math.floor( (timer / 1000 / 60 ) % 60 );
+		// var elapsed = '0.0';
+		// timer += 100;
+		// elapsed = Math.floor(time/100) / 10;
+		// if(Math.round(elapsed) == elapsed){ elapsed +='.0';}
+		// var diff = (time - elapsed) - timer;
+		// if (timer <= 500 ){
+		// 	timer = 0;
+		// }
+		return {
+			'total': timer,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	}
+
+	function workoutProgram(){
+		if ( cycle !== 0 && paused === false && runWorkout === 1 && timer >= 0){
+			var time = timer;
+			// setInterval(test);
+			var time = getTimeRemaining(timer); 
+			// console.log(time);
+			$('#timer').text(time.minutes + ":" + time.seconds );
+			// $('#timer').css('visibility','visible');
+			$('.exercise-title').show();
+			$('.exercise-icon').css('display','block');
+			$('.exercise-icon').css('visibility','visible');
+			$('.btn-row').hide();
+			$('.stop').show();
+			$('.complete-display').hide();
+			timer -= 999;
+			console.log(timer);
+		}else if (cycle !== 0 && stopped == true){
+			// $('#timer').css('visibility','hidden');
+			console.log('Stopped');
+			console.log('Trolling ya homie');
+			console.log(cycle);
+			console.log(timer);
+			$('.stop').hide();
+			$('.btn-row').show();
+			// isPaused = false;
+			// cycle=0;
+			// clearInterval(clock);
+			// $('#timer').hide;
+		}else{
+			console.log("You having a bad time homie");
+			console.log(time);
+			};	
+
+		if (timer <= 0 && hideComplete === false){
+			$('.exercise-icon').css('visibility','hidden');
+			$('.complete-display').show();
+			// $('#timer').css('visibility','hidden');
+			$('.stop').hide();
+			$('.btn-row').show();
+			clearInterval(clock);
+			console.log(cycle);
+		}else{
+			$('.preworkout').css('visibility','hidden');
+			$('.motivation').css('visibility','hidden' );
+		};
+	};//END RUN WORKOUT
+
+
+	// get the local storage user
+
+	
+
+	//USER SETTINGS - ROUTINES
 	if(currentUser.runWorkout){
 		runWorkout = currentUser.runWorkout;
 	}else(updateUser("runWorkout",'0'))
@@ -32,15 +120,12 @@ $(document).ready(function(){
 	if (currentUser.cycle){
 		cycle = currentUser.cycle;
 	}else(updateUser("cycle",'0'))
-	// if the current user has a saved mood, update the icon
-	// if (currentUser.mood){
-	// 	$('.exercise-icon').html('<img src="img/'+currentUser.mood+'.png">');
-	// }else (updateUser("mood",'dot') ) //DEFAULT MODE
 
 	if (currentUser.exImg){
 		$('.exercise-icon').html('<img src="img/dot.png">');
 	}else (updateUser("exImg",'dot') )
 
+	//EXERCISE SAVED DATA
 	if(currentUser.pushupcount1){
 		pushupcount1 = currentUser.pushupcount1;
 	}else(updateUser("pushupcount1",pushUp1))
@@ -69,57 +154,35 @@ $(document).ready(function(){
 	//DEFAULT SETTINGS
 	$('.btn-row').hide();
 
-	var clock = setInterval(workoutProgram, 999);
+	//SETTING TIMERS
 
-		if ( cycle === 1 || cycle === 2 ){
-			var delayWorkout = setTimeout(delay,9000);
-			var prepareDelay = setTimeout(prepareDelay1,6000);
+	// if ( cycle === 1 || cycle === 2 ){
+	// 	var delayWorkout = setTimeout(delay,9000);
+	// 	var prepareDelay = setTimeout(prepareDelay1,6000);
 
-		}else if ( cycle === 3 || cycle === 4){
-			var delayWorkout2 = setTimeout(delay2,9000);
-			var prepareDelay2 = setTimeout(prepareDelay2,8000);
-		}else if ( cycle === 5 || cycle === 6){
-			var delayWorkout3 = setTimeout(delay3,6950);
-			var prepareDelay3 = setTimeout(prepareDelay3,6950);
-		}else if (cycle === 7 || cycle === 8){
-			var delayWorkout4 = setTimeout(delay4,6050);
-			var prepareDelay4 = setTimeout(prepareDelay4,6050);
-		}
-		 else if (cycle === 9 || cycle === 10){
-			var delayWorkout5 = setTimeout(delay5,11500);
-		}else if (cycle === 11 || cycle === 12){
-			var delayWorkout6 = setTimeout(delay6,6000);
-		}else if (cycle === 13 || cycle === 14){
-			var delayWorkout7 = setTimeout(delay7,6000);
-		}else if (cycle === 15 || cycle === 16){
-			var delayWorkout8 = setTimeout(delay8,6000);
-		}
+	// }else if ( cycle === 3 || cycle === 4){
+	// 	var delayWorkout2 = setTimeout(delay2,9000);
+	// 	var prepareDelay2 = setTimeout(prepareDelay2,8000);
+	// }else if ( cycle === 5 || cycle === 6){
+	// 	var delayWorkout3 = setTimeout(delay3,6950);
+	// 	var prepareDelay3 = setTimeout(prepareDelay3,6950);
+	// }else if (cycle === 7 || cycle === 8){
+	// 	var delayWorkout4 = setTimeout(delay4,6050);
+	// 	var prepareDelay4 = setTimeout(prepareDelay4,6050);
+	// }
+	//  else if (cycle === 9 || cycle === 10){
+	// 	var delayWorkout5 = setTimeout(delay5,11500);
+	// }else if (cycle === 11 || cycle === 12){
+	// 	var delayWorkout6 = setTimeout(delay6,6000);
+	// }else if (cycle === 13 || cycle === 14){
+	// 	var delayWorkout7 = setTimeout(delay7,6000);
+	// }else if (cycle === 15 || cycle === 16){
+	// 	var delayWorkout8 = setTimeout(delay8,6000);
+	// }
 
-	
-
-	//Time Calculator
-	function getTimeRemaining(endtime){
-		//Date.parse is converting to milliseconds. Essentially I'm trying to build a calculator with the var time
-		var time = timer;
-		var seconds = Math.floor( (timer/1000) % 60 );
-		var minutes = Math.floor( (timer / 1000 / 60 ) % 60 );
-		// var elapsed = '0.0';
-		// timer += 100;
-		// elapsed = Math.floor(time/100) / 10;
-		// if(Math.round(elapsed) == elapsed){ elapsed +='.0';}
-		// var diff = (time - elapsed) - timer;
-		// if (timer <= 500 ){
-		// 	timer = 0;
-		// }
-		return {
-			'total': timer,
-			'minutes': minutes,
-			'seconds': seconds
-		};
-	}
 
 	//Button Controls
-			$('.stop-btn').click(function(){
+		$('.stop-btn').click(function(){
 			cycle = 0;
 			$('#timer').html('Stopped');
 			updateUser("cycle",cycle);
@@ -153,72 +216,83 @@ $(document).ready(function(){
 			setInterval(workoutProgram,1000)
 		});
 
-	//LEVEL SELECTOR
-
-	if (runWorkout === 1){
+	//EXERCISE ROUTINE CYCLE
+	if (runWorkout == 1){
 		if( cycle == 1 ){
 			cycle = 1;
-			updateUser("cycle",cycle);
+			// updateUser("cycle",cycle);
+			var clock = setInterval(workoutProgram, 999);
+			var delayWorkout = setTimeout(delay,9000);
+			var prepareDelay = setTimeout(prepareDelay1,6000);
 			console.log("level 1"); //console log just level to test
 			 // imageEnter(sendUp);
-			var timer = 5000;
+			timer = 5000;
 			// $('.exercise-title').hide();
 			// $('.exercise-icon').hide();
 			$('.btn-row').hide();
 			delaySwitch = true;
 			hideComplete = true;
+			// if ( timer >= 0){
+			// 	prepare = true;
+			// }
 
-		
 			function prepareDelay1(){
 				$('.exercise-title').html('Prepare');
 				$('#timer').css('visibility','hidden');
 				$('.exercise-icon').html('<img class="workout-img" src="img/Pushups.gif">');
 				timer = 1999;
+				console.log("I work the prepare delay");
 			};
 				function delay(){
 					if (delaySwitch === true && cycle === 1){
-					clock = clearInterval(clock);
-					cycle = 2;
-					$('.exercise-title').html('Pushups');
-					$('.exercise-icon').show();
-					timer = 0;
-					timer = 12000;
-					clock = setInterval(workoutProgram, 999);
-					$('#timer').css('visibility','visible');
-					console.log("pushy pushy");
-					
-					hideComplete = false;
-					// delaySwitch = false;
-					$('.next-form').html('<form id="exercise-count"> <input id="pushupcount1" type="number" name="pushupcount"></form>')
-					$('.next').html('<a class="exercise3 next-btn" href="timer.html">Level Up</a>');
-					cycle = 3;
-					// var delayWorkout = setTimeout(delay,5950);
-					updateUser("cycle",cycle);
-
-					$(".exercise3.next-btn").click(function(){
-						delaySwitch = false;
-						pushUp1 = document.getElementById("pushupcount1").value;
-						console.log("I work");
-						updateUser("pushupcount1", pushUp1);
-
+						clock = clearInterval(clock);
+						cycle = 2;
+						updateUser("cycle",cycle);
+						$('.exercise-title').html('Pushups');
+						$('.exercise-icon').show();
+						timer = 0;
+						timer = 12000;
+						clock = setInterval(workoutProgram, 999);
+						$('#timer').css('visibility','visible');
+						console.log("pushy pushy");
+						
+						hideComplete = false;
+						// delaySwitch = false;
+						$('.next-form').html('<form id="exercise-count"> <input id="pushupcount1" type="number" name="pushupcount"></form>')
+						$('.next').html('<a class="exercise3 next-btn" href="timer.html">Level Up</a>');
+						
+						// var delayWorkout = setTimeout(delay,5950);
+						
+						cycle = 3;
+						updateUser("cycle",cycle);
+						$(".exercise3.next-btn").click(function(){
+							// delaySwitch = false;
+							pushUp1 = document.getElementById("pushupcount1").value;
+							console.log("I work");
+							updateUser("pushupcount1", pushUp1);
 						});
-				}else{
-					console.log('No delay');
-				}
-			};
-					
+					}else{
+						console.log('No delay');
+					}
+				};				
 
 		}else if ( cycle == 3 ){
-			console.log("level 3");
-			delaySwitch = true;
+			$('#timer').css('visibility','visible');
 			timer = 7000;
+			cycle = 3
+			updateUser("cycle",cycle);
+			var clock = setInterval(workoutProgram, 999);
+			var delayWorkout = setTimeout(delay2,11000);
+			prepareDelay2 = setTimeout(prepareDelay2,8000);
+			delaySwitch = true;
 			$('.exercise-title').html('Jab/Cross');
 			$('.exercise-icon').html('<img class="workout-img" src="img/JabCross.gif">');
-			$('.exercise-icon').css('visibility','show');
+			$('.exercise-icon').css('visibility','visible');
+			hideComplete = true;
 
 			function prepareDelay2(){
 				$('.exercise-title').html('Prepare');
-				$('#timer').css('visibility','hidden');
+				 $('#timer').css('visibility','hidden');
 				$('.exercise-icon').html('<img class="workout-img" src="img/MilitaryPushups.gif">');
 				timer = 1999;
 			};
@@ -227,14 +301,15 @@ $(document).ready(function(){
 				if (delaySwitch === true && cycle === 3){
 					clock = clearInterval(clock);
 					cycle = 4;
+					updateUser("cycle",cycle);
 					$('.exercise-title').html('Military Pushups');
-					$('#timer').css('visibility','visible');
-
+					$('.exercise-icon').show();
+					timer = 0;
 					timer = 9000;
 					console.log("Delay 2");
 					clock = setInterval(workoutProgram, 999);
-					$('.exercise-icon').html('<img class="workout-img" src="img/MilitaryPushups.gif">');
-					console.log("I am the timed cycle 4 :)");
+					$('#timer').css('visibility','visible');
+
 					hideComplete = false;
 					$('.next-form').html('<form id="exercise-count"> <input id="militarycount1" type="number" name="militarycount"></form>')
 					$('.next').html('<a class="exercise5 next-btn" href="timer.html">Level Up</a>');
@@ -250,11 +325,18 @@ $(document).ready(function(){
 				}
 			};
 			}else if ( cycle == 5 ){
-				console.log("level5");
-				delaySwitch = true;
+				$('#timer').css('visibility','visible');
 				timer = 7000;
+				cycle = 5;
+				updateUser("cycle",cycle);
+				var clock = setInterval(workoutProgram, 999);
+				var delayWorkout = setTimeout(delay3,11000);
+			 	prepareDelay3 = setTimeout(prepareDelay3,8050);
+				delaySwitch = true;
 				$('.exercise-title').html('Hook/Uppercut');
 				$('.exercise-icon').html('<img class="workout-img" src="img/HookUppercut.gif">');
+				$('.exercise-icon').css('visibility','visible');
+				hideComplete = true;
 
 				function prepareDelay3(){
 					$('.exercise-title').html('Prepare');
@@ -267,14 +349,16 @@ $(document).ready(function(){
 					if(delaySwitch === true && cycle === 5){
 						clock = clearInterval(clock);
 					cycle = 6;
+					updateUser("cycle",cycle);
 					$('.exercise-title').html('Wide Pushups');
+					$('.exercise-icon').show();
 					$('#timer').css('visibility','visible');
-
+					timer = 0;
 					timer = 7000;
 					clock = setInterval(workoutProgram, 999);
-					$('.exercise-icon').html('<img class="workout-img" src="img/WidePushups.gif">');
-					console.log("I am the timed cycle 6");
+					$('#timer').css('visibility','visible');
 					hideComplete = false;
+
 					$('.next-form').html('<form id="exercise-count"> <input id="widecount1" type="number" name="widecount"></form>')
 					$('.next').html('<a class="exercise7 next-btn" href="timer.html">Level Up</a>');
 					cycle = 7;
@@ -288,11 +372,17 @@ $(document).ready(function(){
 					}
 			};
 			}else if ( cycle == 7 ){
-				console.log("level7");
-				delaySwitch = true;
+				$('#timer').css('visibility','visible');
 				timer = 7000;
+				cycle = 7;
+				updateUser("cycle",cycle);
+				console.log("level7");
+				var delayWorkout = setTimeout(delay4,11000);
+			 	prepareDelay4 = setTimeout(prepareDelay4,8000);
+				delaySwitch = true;
 				$('.exercise-title').html('Knee Strikes');
 				$('.exercise-icon').html('<img class="workout-img" src="img/Knees.gif">');
+				$('.exercise-icon').css('visibility','visible');
 
 				function prepareDelay4(){
 					$('.exercise-title').html('Prepare');
@@ -308,12 +398,12 @@ $(document).ready(function(){
 					$('.exercise-title').html('Dips');
 					$('.exercise-icon').show();
 					$('#timer').css('visibility','visible');
-
+					timer = 0;
 					timer = 7000;
 					clock = setInterval(workoutProgram, 999);
-					$('.exercise-icon').html('<img class="workout-img" src="img/Dips.gif">');
-					console.log("I am the timed cycle 8");
+					$('#timer').css('visibility','visible');
 					hideComplete = false;
+
 					$('.next-form').html('<form id="exercise-count"> <input id="dipcount1" type="number" name="dipcount"></form>')
 					$('.next').html('<a class="exercise9 next-btn" href="timer.html">Level Up</a>');
 					cycle = 9;
@@ -327,10 +417,16 @@ $(document).ready(function(){
 					}
 			};
 			}else if ( cycle == 9 ){
-				delaySwitch = true;
 				timer = 12000;
+				cycle = 3
+				updateUser("cycle",cycle);
+				var clock = setInterval(workoutProgram, 999);
+				var delayWorkout = setTimeout(delay2,11000);
+				prepareDelay5 = setTimeout(prepareDelay5,8000);
+				delaySwitch = true;
 				$('exercise-title').html('Break!');
 				$('exercise-icon').show();
+
 				function delay5(){
 					if(delaySwitch === true && cycle === 9){
 					clock = clearInterval(clock);
@@ -459,28 +555,28 @@ $(document).ready(function(){
 			
 		});
 
-		// $('.exercise2').click(function(){
-		// 	cycle = 2;
-		// 	updateUser("cycle",cycle);
-		// 	timer = 6000;
-		// 	$('.exercise-title').html('Pushups');
-		// 	clock = setInterval(workoutProgram, 1000);
-		// 	console.log('I am running from the button');
-		// 	$('.next').html('<a class="exercise3 next-btn" href="timer.html">Level Up</a>');
-		// });
-
-		$('.exercise3').click(function(){
-			cycle = 3;
+		$('.exercise2').click(function(){
+			cycle = 2;
 			updateUser("cycle",cycle);
-			$('.exercise-title').html('Punch Cycle');
-			$('.next').html('<a class="exercise3 next-btn">Level Up</a>');
-			console.log(cycle);
-			console.log('I am exercise three');
-			timer = 9000;
+			timer = 6000;
+			$('.exercise-title').html('Pushups');
 			clock = setInterval(workoutProgram, 1000);
-			console.log(timer);
-
+			console.log('I am running from the button');
+			$('.next').html('<a class="exercise3 next-btn" href="timer.html">Level Up</a>');
 		});
+
+		// $('.exercise3').click(function(){
+		// 	cycle = 3;
+		// 	updateUser("cycle",cycle);
+		// 	$('.exercise-title').html('Punch Cycle');
+		// 	$('.next').html('<a class="exercise3 next-btn">Level Up</a>');
+		// 	console.log(cycle);
+		// 	console.log('I am exercise three');
+		// 	timer = 9000;
+		// 	clock = setInterval(workoutProgram, 1000);
+		// 	console.log(timer);
+
+		// });
 
 		$('.exercise4').click(function(){
 			cycle = 4;
@@ -492,90 +588,24 @@ $(document).ready(function(){
 			updateUser("cycle",cycle);
 		});
 
-		
-		function workoutProgram(){
-			if ( cycle !== 0 && paused === false && runWorkout === 1 && timer >= 0){
-				var time = timer;
-				// setInterval(test);
-				var time = getTimeRemaining(timer); 
-				// console.log(time);
-				$('#timer').text(time.minutes + ":" + time.seconds );
-				$('#timer').show();
-				$('.exercise-title').show();
-				$('.exercise-icon').css('display','block');
-				$('.exercise-icon').css('visibility','visible');
-				$('.btn-row').hide();
-				$('.stop').show();
-				$('.complete-display').hide();
-				timer -= 999;
-				console.log(timer);
-
-				if (timer === 0 && hideComplete === false){
-					$('.exercise-icon').css('visibility','hidden');
-					$('.complete-display').show();
-					$('#timer').css('visibility','hidden');
-					$('.stop').hide();
-					$('.btn-row').show();
-					clearInterval(clock);
-					console.log(cycle);
-				}else if (prepare === true){
-					function prepareDelay(){
-						$('.prepare').show();
-
-					}
-
-				}else{
-					$('.preworkout').css('visibility','hidden');
-					$('.motivation').css('visibility','hidden' );
-				};
-
-				if (timer === 0 && cycle === 0){
-					clearInterval(clock);
-
-				}
-			}else{
-				$('#timer').css('visibility','hidden');
-				console.log('Stopped');
-				$('.stop').hide();
-				$('.btn-row').show();
-				// isPaused = false;
-				cycle=0;
-				clearInterval(clock);
-				// $('#timer').hide;
-			}
-		}
-	};//END RUN WORKOUT
+	
+};
+});
 
 	//IMAGE//
 	
-		$('.dot').click(function(){
-			$('.exercise-icon').html('<img src="img/dot.png">');
-			updateUser("exImg",'dot');
-		});
+		// $('.dot').click(function(){
+		// 	$('.exercise-icon').html('<img src="img/dot.png">');
+		// 	updateUser("exImg",'dot');
+		// };
 
-		$('.heart').click(function(){
-			$('.exercise-icon').html('<img src="img/heart.png">');
-			updateUser("mood",'heart');
-		});
-	});
+		// $('.heart').click(function(){
+		// 	$('.exercise-icon').html('<img src="img/heart.png">');
+		// 	updateUser("mood",'heart');
+		// };
 	
-	/**USER SET UP**/
-	function updateUser(setting,value){
-		var user = JSON.parse(localStorage.getItem('user'));
-		if (!user){
-			user = {};
-		}
-		user[setting] = value; // user.mood
-		localStorage.setItem('user', JSON.stringify(user));
-	}
+	
 
-	function getUser(){
-		var user = JSON.parse(localStorage.getItem('user'));
-		if (!user){
-			return {};
-		}
-		return user;
-	}
 
 	/**AUDIO PLAYER**/
 	// $('.audio1').click(function(){
